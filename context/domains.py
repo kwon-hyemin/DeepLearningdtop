fe961b7789db888012525306c79a2ea3921cccad
kwon-hyemin <kwon9184h@gmail.com>
2022-06-26 17:55:57 +0900

    0626

--- context/domains.py
+++ context/domains.py
@@ -1,57 +1,155 @@
 # context, fname, train, test, id, label
+import json
 from dataclasses import dataclass
+from abc import *
+import pandas as pd
+from typing import TypeVar
 
-import pandas
+PandasDataFrame = TypeVar('pandas.core.frame.DataFrame')
+GooglemapsClient = TypeVar('googlemaps.Client')
 
 
-@dataclass()
+@dataclass
 class Dataset:
-    data_path: str
-    save_path: str
-    file_name: str
-    train: pandas.core.frame.DataFrame
-    test: pandas.core.frame.DataFrame
+    dname: str
+    sname: str
+    fname: str
+    train: str
+    test: str
     id: str
     label: str
 
     @property
-    def data_path(self) -> str: return self._data_path
+    def dname(self) -> str: return self._dname
 
-    @data_path.setter
-    def data_path(self, value): self._data_path = value
-
-    @property
-    def save_path(self) -> str: return self._save_path
-
-    @save_path.setter
-    def save_path(self, save_path): self._save_path = save_path
+    @dname.setter
+    def dname(self, dname): self._dname = dname
 
     @property
-    def file_name(self) -> str: return self._file_name
+    def sname(self) -> str: return self._sname
 
-    @file_name.setter
-    def file_name(self, value): self._file_name = value
+    @sname.setter
+    def sname(self, sname): self._sname = sname
 
     @property
-    def train(self) -> pandas.core.frame.DataFrame: return self._train
+    def fname(self) -> str: return self._fname
+
+    @fname.setter
+    def fname(self, fname): self._fname = fname
+
+    @property
+    def train(self) -> str: return self._train
 
     @train.setter
-    def train(self, value): self._train = value
+    def train(self, train): self._train = train
 
     @property
-    def test(self) -> pandas.core.frame.DataFrame: return self._test
+    def test(self) -> str: return self._test
 
     @test.setter
-    def test(self, value): self._test = value
+    def test(self, test): self._test = test
 
     @property
     def id(self) -> str: return self._id
 
     @id.setter
-    def id(self, value): self._id = value
+    def id(self, id): self._id = id
 
     @property
     def label(self) -> str: return self._label
 
     @label.setter
-    def label(self, value): self._label = value
+    def label(self, label): self._label = label
+
+
+@dataclass
+class File(object):
+    context: str
+    fname: str
+    dframe: object
+
+    @property
+    def context(self) -> str: return self._context
+
+    @context.setter
+    def context(self, context): self._context = context
+
+    @property
+    def fname(self) -> str: return self._fname
+
+    @fname.setter
+    def fname(self, fname): self._fname = fname
+
+    @property
+    def dframe(self) -> str: return self._dframe
+
+    @dframe.setter
+    def dframe(self, dframe): self._dframe = dframe
+
+
+class PrinterBase(metaclass=ABCMeta):
+    @abstractmethod
+    def dframe(self):
+        pass
+    # new_file, csv, xls, json
+
+
+class ReaderBase(metaclass=ABCMeta):
+    @abstractmethod
+    def new_file(self, file) -> str:
+        pass
+
+    @abstractmethod
+    def csv(self) -> object:
+        pass
+
+    @abstractmethod
+    def xls(self) -> object:
+        pass
+
+    @abstractmethod
+    def json(self) -> object:
+        pass
+
+
+# Reader
+# Printer
+class Reader(ReaderBase):
+    def new_file(self, file) -> str:
+        return file.context + file.fname
+
+    # file.context = './data/'
+    # file.fname = 'cctv_in_seoul'
+    # file 객체에 있는 context와 fname이 필요하다.
+
+    def csv(self, path: str) -> PandasDataFrame:
+        o = pd.read_csv(f'{self.new_file(path)}', encoding='UTF-8', thousands=',')
+        print(f'type: {type(o)}')
+        return o
+
+    def xlsx(self, file, header) -> str:
+        return pd.read_excel(f'{self.new_file(file)}.xlsx', header=header)
+
+    def xls(self, path: str, header: str, cols: str, skiprows) -> PandasDataFrame:
+        return pd.read_excel(f'{self.new_file(path)}.xls', header=header, usecols=cols, skiprows=skiprows)
+
+    def json(self, path: str) -> PandasDataFrame:
+        return pd.read_json(f'{self.new_file(path)}.json', encoding='UTF-8')
+
+    def map_json(self, path: str) -> object:
+        return json.load(open(f'{self.new_file(path)}.json', encoding='UTF-8'))
+
+    @staticmethod
+    def gmaps() -> GooglemapsClient:
+        a = googlemaps.Client(key='')
+        print(type(a))
+        return a
+
+    def myprint(self, this):
+        print('*' * 100)
+        print(f'1. Target type \n {type(this)} ')
+        print(f'2. Target column \n {this.columns} ')
+        print(f'3. Target top 1개 행\n {this.head(1)} ')
+        print(f'4. Target bottom 1개 행\n {this.tail(1)} ')
+        print(f'4. Target null 의 갯수\n {this.isnull().sum()}개')
+        print('*' * 100)
